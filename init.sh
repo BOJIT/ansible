daf5ff711f32ec1ff1706cc5dd432fce282cc8f2
jpedley <james.Bennion-Pedley@dyson.com>
2023-01-04 16:35:49 +0000

    Install ansible at system level

--- init.sh
+++ init.sh
@@ -8,13 +8,18 @@
 
 set -e
 
+cd ~
+
 # Bare minimum, need python3, pip, curl and git
 sudo apt-get install -y curl git
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
+python3 get-pip.py --no-warn-script
+rm ~/get-pip.py
 
 # Install ansible and temporarily add to PATH
-python3 -m pip install --user ansible
+python3 -m pip install ansible --no-warn-script
 PATH=$PATH:~/.local/bin
 
+# Ansible handles the rest!
+echo "##########################################"
 ansible-pull -U https://github.com/BOJIT/ansible.git

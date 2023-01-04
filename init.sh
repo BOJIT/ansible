c7d6fe03f50966f4e413003ece6152a65e480321
jpedley <james.Bennion-Pedley@dyson.com>
2023-01-04 16:39:35 +0000

    tweaks

--- init.sh
+++ init.sh
@@ -13,11 +13,11 @@ cd ~
 # Bare minimum, need python3, pip, curl and git
 sudo apt-get install -y curl git
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --no-warn-script
+python3 get-pip.py --user --no-warn-script
 rm ~/get-pip.py
 
 # Install ansible and temporarily add to PATH
-python3 -m pip install ansible --no-warn-script
+python3 -m pip install --user ansible --no-warn-script
 PATH=$PATH:~/.local/bin
 
 # Ansible handles the rest!

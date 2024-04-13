db85cd3039bf5d3e2a24afee289a33cbe59217e3
BOJIT <35117353+BOJIT@users.noreply.github.com>
2024-04-13 22:42:16 +0800

    Fix macos packages + change profile name

--- roles/desktop/files/iterm_load.py
+++ roles/desktop/files/iterm_load.py
@@ -3,7 +3,7 @@ import iterm2
 async def main(connection):
     all_profiles = await iterm2.PartialProfile.async_query(connection)
     for profile in all_profiles:
-        if profile.name == "Default":
+        if profile.name == "BOJIT":
             await profile.async_make_default()
             return
 

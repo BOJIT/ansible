b61e794a2e2c1f11279ccf292693fe6d27829b13
BOJIT <35117353+BOJIT@users.noreply.github.com>
2024-04-13 22:13:39 +0800

    Add iTerm2 customisations

---
+++ roles/desktop/files/iterm_load.py
@@ -0,0 +1,10 @@
+import iterm2
+
+async def main(connection):
+    all_profiles = await iterm2.PartialProfile.async_query(connection)
+    for profile in all_profiles:
+        if profile.name == "Default":
+            await profile.async_make_default()
+            return
+
+iterm2.run_until_complete(main)
